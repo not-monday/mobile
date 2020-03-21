1372a666123942ef0d0a1e1d33daa8e1fb1126c0
13abel <therichardwei@gmail.com>
2020-03-20 23:49:04 -0400

    added test lib and skeleton for active workout

---
+++ test/bloc/active_workout_bloc.dart
@@ -0,0 +1,12 @@
+import 'package:test/test.dart';
+
+
+void main() {
+  test("complete exercise updates workout record", ()=>{
+
+  });
+
+  test("fail exercise updates workout record", ()=>{
+
+  });
+}
