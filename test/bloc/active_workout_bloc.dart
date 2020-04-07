08787fc0a7c657ccc67a1b40de4bbaeabe86d9b7
13abel <therichardwei@gmail.com>
2020-04-07 15:58:30 -0400

    added tests for the remaining and completed workout count

--- test/bloc/active_workout_bloc.dart
+++ test/bloc/active_workout_bloc.dart
@@ -59,6 +59,59 @@ void main() {
 
     activeWorkoutBloc.add(FailExerciseEvent(2));
   });
+
+  test("completed workout count is not incremented until all sets of an exercise are complete", () {
+    var mockWorkout = WorkoutRepositoryImpl.mockWorkout();
+    when(mockWorkoutRepo.retrieveWorkout()).thenAnswer((_) => Future.value(mockWorkout));
+
+    var matcher = TypeMatcher<ActiveWorkoutState>();
+
+    var matchEndState = (_) {
+      var failedWorkoutSet = activeWorkoutBloc.state.setRecords[0][0];
+      expect(failedWorkoutSet.repsBeforeFailure, 2);
+    };
+
+    expectLater(
+        activeWorkoutBloc,
+        emitsInOrder([
+          matcher.having((state) => state.workoutRef, "initially no workout ref", null),
+          matcher.having((state) => state.completedExerciseCount, "initial", 0),
+          matcher
+              .having((state) => state.completedExerciseCount, "0/3 sets completed", 0)
+              .having((state) => state.remainingExerciseCount, "3/3 exercises remaining", 3),
+    ])).then((_) => matchEndState);
+
+    activeWorkoutBloc.add(CompleteExerciseEvent());
+  });
+
+  test("completed workout count is incremented when all sets are completed for an exercise", () {
+    var mockWorkout = WorkoutRepositoryImpl.mockWorkout();
+    when(mockWorkoutRepo.retrieveWorkout()).thenAnswer((_) => Future.value(mockWorkout));
+
+    var matcher = TypeMatcher<ActiveWorkoutState>();
+
+    var matchEndState = (_) {
+      var failedWorkoutSet = activeWorkoutBloc.state.setRecords[0][0];
+      expect(failedWorkoutSet.repsBeforeFailure, 2);
+    };
+
+    expectLater(
+        activeWorkoutBloc,
+        emitsInOrder([
+          matcher.having((state) => state.workoutRef, "initially no workout ref", null),
+          matcher.having((state) => state.completedExerciseCount, "initial", 0),
+          matcher.having((state) => state.completedExerciseCount, "1/3 sets completed", 0),
+          matcher.having((state) => state.completedExerciseCount, "2/3 sets completed", 0),
+          matcher
+              .having((state) => state.completedExerciseCount, "3/3 sets completed", 1)
+              .having((state) => state.remainingExerciseCount, "2/3 exercises remaining", 2),
+
+        ])).then((_) => matchEndState);
+
+    activeWorkoutBloc.add(CompleteExerciseEvent());
+    activeWorkoutBloc.add(CompleteExerciseEvent());
+    activeWorkoutBloc.add(CompleteExerciseEvent());
+  });
 }
 
 class MockWorkoutRepo extends Mock implements WorkoutRepository {}
