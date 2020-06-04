e7ae0a06f98cc70c6a4bfa256ca134cd9b1d1f49
Raahul Sivanathan <raahul.sivanathan@gmail.com>
2020-06-03 22:37:29 -0400

    Create class to deal with menu item building
    Fixed the problem with button menu still open after clicking
    an option.

--- lib/presentation/workout_actions/workout_exercise_card.dart
+++ lib/presentation/workout_actions/workout_exercise_card.dart
@@ -2,10 +2,16 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:stronk/domain/constants.dart' as Constants;
 import 'package:stronk/domain/model/workout.dart';
+import 'package:stronk/presentation/workout_actions/PopUpMenu.dart';
 import 'package:stronk/presentation/workout_actions/workout_action_bloc.dart';
 import 'package:stronk/presentation/workout_actions/workout_action_exerciseSetDisplay.dart';
 
-enum workoutOptions { editWorkoutName, editWorkoutDescription }
+enum workoutOptions {
+  editWorkoutName,
+  editWorkoutDescription,
+  deleteWorkoutExercise,
+  addWorkoutExercise
+}
 
 class WorkoutExerciseCard extends StatelessWidget {
   final WorkoutExercise workoutExercise;
@@ -38,55 +44,7 @@ class WorkoutExerciseCard extends StatelessWidget {
                 mainAxisSize: MainAxisSize.min,
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: <Widget>[
-                  ListTile(
-                    trailing: PopupMenuButton<workoutOptions>(
-                        itemBuilder: (BuildContext context) =>
-                            <PopupMenuEntry<workoutOptions>>[
-                              PopupMenuItem<workoutOptions>(
-                                  value: workoutOptions.editWorkoutDescription,
-                                  child: FlatButton(
-                                    child: Text("Edit Workout Description"),
-                                    onPressed: () => {
-                                      bloc.add(new EditWorkoutEvent(
-                                          workoutId: workoutId,
-                                          editAction: Constants
-                                              .EDIT_WORKOUT_DESCRIPTION,
-                                          newValue: "new description"))
-                                    },
-                                  )),
-                              PopupMenuItem<workoutOptions>(
-                                  value: workoutOptions.editWorkoutName,
-                                  child: FlatButton(
-                                      child: Text("Edit Workout Name"),
-                                      onPressed: () => {
-                                            bloc.add(new EditWorkoutEvent(
-                                                workoutId: workoutId,
-                                                editAction:
-                                                    Constants.EDIT_WORKOUT_NAME,
-                                                newValue: "new workout Name"))
-                                          })),
-                              PopupMenuItem<workoutOptions>(
-                                  child: FlatButton(
-                                child: Text("Delete WorkoutExercise"),
-                                onPressed: () => {
-                                  bloc.add(new WorkoutExercisesEvent(
-                                      workoutId: workoutId,
-                                      workoutExerciseId: workoutExercise.id,
-                                      action: Constants.DELETE_ACTION))
-                                },
-                              )),
-                              PopupMenuItem<workoutOptions>(
-                                  child: FlatButton(
-                                child: Text("Add workout exercise"),
-                                onPressed: () => {
-                                  bloc.add(new WorkoutExercisesEvent(
-                                      workoutId: workoutId,
-                                      workoutExerciseId: "1000",
-                                      action: Constants.ADD_ACTION))
-                                },
-                              ))
-                            ]),
-                  ),
+                  ListTile(trailing: buildPopUpMenu(context)),
                   Align(
                     alignment: Alignment.centerLeft,
                     child: Container(
@@ -98,5 +56,42 @@ class WorkoutExerciseCard extends StatelessWidget {
                 ]),
           ),
         ));
+  }
+
+  Widget buildPopUpMenu(BuildContext context) {
+    return PopUpMenu.createPopup([
+      new PopUpMenu(
+          option: "Edit Workout Description",
+          onTap: () => {
+                bloc.add(new EditWorkoutEvent(
+                    workoutId: workoutId,
+                    editAction: Constants.EDIT_WORKOUT_DESCRIPTION,
+                    newValue: "new description"))
+              }),
+      new PopUpMenu(
+          option: "Edit Workout Name",
+          onTap: () => {
+                bloc.add(new EditWorkoutEvent(
+                    workoutId: workoutId,
+                    editAction: Constants.EDIT_WORKOUT_NAME,
+                    newValue: "new workout Name"))
+              }),
+      new PopUpMenu(
+          option: "Delete WorkoutExercise",
+          onTap: () => {
+                bloc.add(new WorkoutExercisesEvent(
+                    workoutId: workoutId,
+                    workoutExerciseId: workoutExercise.id,
+                    action: Constants.DELETE_ACTION))
+              }),
+      new PopUpMenu(
+          option: "Add workout exercise",
+          onTap: () => {
+                bloc.add(new WorkoutExercisesEvent(
+                    workoutId: workoutId,
+                    workoutExerciseId: "1000",
+                    action: Constants.ADD_ACTION))
+              })
+    ]);
   }
 }
